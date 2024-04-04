b52c12badcf64131d6cb6f690883fb90c9723ffd
Lambrous <lambrous@outlook.ph>
2024-04-04 10:58:29 +0800

    Add book management functionality

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("hello");
+const addBookForm = document.querySelector(".add-book-form");
+const addBookModal = document.querySelector(".add-book-modal");
+const openModalButton = document.querySelector(".open-btn");
+const closeModalButton = document.querySelector(".close-btn");
+const libraryElement = document.querySelector("ul.library");
+
+const myLibrary = [];
+
+let nextBookID = 1;
+
+function Book(title, author, pages) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.isRead = false;
+	this.id = nextBookID++;
+}
+
+function addBookToLibrary(book) {
+	myLibrary.push(book);
+
+	const bookItem = createBookElement(book);
+	libraryElement.append(bookItem);
+}
+
+function deleteBook(bookID) {
+	const index = myLibrary.findIndex((book) => book.id === bookID);
+	if (index !== -1) {
+		myLibrary.splice(index, 1);
+	}
+}
+
+function createBookElement(book) {
+	const bookItem = document.createElement("li");
+	bookItem.setAttribute("data-id", book.id);
+	bookItem.textContent = `${book.title} by ${book.author}, ${book.pages} pages`;
+
+	const removeBookButton = document.createElement("button");
+	removeBookButton.textContent = "×";
+	removeBookButton.addEventListener("click", () => {
+		deleteBook(book.id);
+		libraryElement.removeChild(bookItem);
+	});
+	bookItem.append(removeBookButton);
+
+	return bookItem;
+}
+
+function handleBookSubmission(event) {
+	event.preventDefault();
+
+	const newBookData = new FormData(this);
+	const { title, author, totalPages } = Object.fromEntries(newBookData);
+	const newBook = new Book(title, author, totalPages);
+
+	addBookToLibrary(newBook);
+	addBookModal.close();
+	event.target.reset();
+}
+
+openModalButton.addEventListener("click", () => addBookModal.showModal());
+closeModalButton.addEventListener("click", () => addBookModal.close());
+addBookForm.addEventListener("submit", handleBookSubmission);
